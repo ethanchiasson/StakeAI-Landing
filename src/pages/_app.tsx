41e4f6b4592fb1618c3cb4c0ea01a8d6febb7a42
ethanchiasson <ethanls@protonmail.com>
2023-07-18 17:53:31 -0500

    added vercel analytics to _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import "@/styles/Globals.scss";
 import type { AppProps } from "next/app";
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -9,6 +10,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <Navbar />
       <Component {...pageProps} />
       <Footer/>
+      <Analytics />
     </>
   );
 }
