4c49cbde465c53c7ce18bd71e79b605c1d2daf39
ethanchiasson <ethanls@protonmail.com>
2023-11-01 11:44:56 -0500

    redirect

---
+++ src/pages/discord/index.tsx
@@ -0,0 +1,25 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+const DiscordRedirect = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    // Redirect to your Discord link after a short delay (e.g., 2 seconds)
+    const redirectTimer = setTimeout(() => {
+      router.push('https://discord.gg/2WDHDnbQvx');
+    }, 2000);
+
+    // Clean up the timer when the component unmounts
+    return () => clearTimeout(redirectTimer);
+  }, []);
+
+  return (
+    <div>
+      {/* <p>Redirecting to Discord...</p> */}
+      {/* You can add a loading message or spinner here if you want */}
+    </div>
+  );
+};
+
+export default DiscordRedirect;
