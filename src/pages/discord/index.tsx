0ed5987fe8823b2dd7b86d77f2185381908ccc86
ethanchiasson <ethanls@protonmail.com>
2023-11-01 12:49:32 -0500

    updated disord redirect

--- src/pages/discord/index.tsx
+++ src/pages/discord/index.tsx
@@ -1,5 +1,11 @@
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
+import styles from "@/styles/Home.module.scss";
+
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
 
 const DiscordRedirect = () => {
   const router = useRouter();
@@ -15,10 +21,12 @@ const DiscordRedirect = () => {
   }, []);
 
   return (
+    <main className={styles.main}>
     <div>
       <p>Redirecting to Discord...</p>
       {/* You can add a loading message or spinner here if you want */}
     </div>
+    </main>
   );
 };
 
